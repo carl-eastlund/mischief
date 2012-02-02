b7e2891042ef0c013e667426839f913fd6104f81
Carl Eastlund <cce@racket-lang.org>
2012-02-02 16:57:04 -0500

    Module path resolution now takes a namespace argument.

--- mischief/racket/module.rkt
+++ mischief/racket/module.rkt
@@ -4,8 +4,10 @@
   resolve-module-path)
 
 (define (resolve-module-path mod-path
+          #:namespace [namespace (current-namespace)]
           #:module-name-resolver [resolver (current-module-name-resolver)]
           #:relative-to [relative-to #false]
           #:syntax-context [syntax-context #false]
           #:load? [load? #true])
-  (resolver mod-path relative-to syntax-context load?))
+  (parameterize {[current-namespace namespace]}
+    (resolver mod-path relative-to syntax-context load?)))
