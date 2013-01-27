77f673338bb48b1031baea637a5236572bcd1e98
Carl Eastlund <cce@racket-lang.org>
2013-01-26 22:41:17 -0500

    Added predicate for resolved module names.

--- mischief/module.rkt
+++ mischief/module.rkt
@@ -1,7 +1,8 @@
 #lang racket/base
 
 (provide
-  resolve-module-path)
+  resolve-module-path
+  module-name?)
 
 (define (resolve-module-path mod-path
           #:namespace [namespace (current-namespace)]
@@ -11,3 +12,16 @@
           #:load? [load? #true])
   (parameterize {[current-namespace namespace]}
     (resolver mod-path relative-to syntax-context load?)))
+
+(define (module-name? x)
+  (cond
+    [(list? x)
+     (and (pair? x) (module-base-name? (car x))
+       (and (pair? (cdr x))
+         (andmap symbol? (cdr x))))]
+    [else (module-base-name? x)]))
+
+(define (module-base-name? x)
+  (cond
+    [(path? x) (complete-path? x)]
+    [else (symbol? x)]))
