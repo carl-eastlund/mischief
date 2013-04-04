fe7119517a4dcd3f5c509735a7b5a5664151c14f
Carl Eastlund <cce@racket-lang.org>
2013-04-04 18:25:58 -0400

    Fixed fresh and format-fresh.
    They were producing unbound identifiers that were unique with respect
    to bound-identifier=?, but not with respect to fresh-identifier=?.

--- mischief/transform.rkt
+++ mischief/transform.rkt
@@ -58,9 +58,9 @@
 
 (define (check-missing-identifier actual expected)
   (define table (make-free-id-table))
-  (for {[id (in-list actual)]}
+  (for {[id (in-list expected)]}
     (dict-set! table id #true))
-  (for/first {[id (in-list expected)]
+  (for/first {[id (in-list actual)]
               #:unless (dict-has-key? table id)}
     id))
 
@@ -142,16 +142,21 @@
           fmt . args)
   (define sym0 (apply format-symbol fmt args))
   (define sym
-    (cond
-      [add-suffix?
-       (define n (add1 (hash-ref fresh:base->count sym0 0)))
-       (begin0 (format-symbol "~a~~~a" sym0 n)
-         ;; update the number of times we've seen this symbol
-         (hash-set! fresh:base->count sym0 n)
-         ;; keep the base in the table as long as its derivatives exist
-         (hash-set! fresh:sym->base sym sym0))]
-      [else sym0]))
-  ((fresh-mark) (to-syntax sym #:source source)))
+    (unintern
+      (cond
+        [add-suffix?
+         (define n (add1 (hash-ref fresh:base->count sym0 0)))
+         (begin0 (format-symbol "~a~~~a" sym0 n)
+           ;; update the number of times we've seen this symbol
+           (hash-set! fresh:base->count sym0 n)
+           ;; keep the base in the table as long as its derivatives exist
+           (hash-set! fresh:sym->base sym sym0))]
+        [else sym0])))
+   ((fresh-mark) (to-syntax sym #:source source)))
+
+(define (unintern sym)
+  (string->uninterned-symbol
+    (symbol->string sym)))
 
 (define fresh:base->count (make-weak-hasheq))
 (define fresh:sym->base (make-weak-hasheq))
