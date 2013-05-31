09bf6b6980ba5e548ed6ca65fe76dc10cb911e73
Carl Eastlund <cce@racket-lang.org>
2013-05-31 17:17:16 -0600

    Filter uninterned symbols out of require/provide.

--- mischief/require.rkt
+++ mischief/require.rkt
@@ -18,12 +18,6 @@
     [(_ mod:id)
      (datum->syntax stx (list #'quote #'mod) stx)]))
 
-(define-syntax-rule (require/provide spec ...)
-  (begin
-    (define-syntax imports (box #f))
-    (require (box-require imports (combine-in spec ...)))
-    (provide (box-provide imports))))
-
 (define-syntax (quote-require stx)
   (syntax-parse stx
     [(_ spec:expr ...)
@@ -32,6 +26,24 @@
        (with-syntax ([(name ...) (map import-local-id imports)])
          (syntax/loc stx '(name ...))))]))
 
+(define-syntax-rule (require/provide spec ...)
+  (begin
+    (define-syntax imports (box #f))
+    (require (box-require imports (combine-in spec ...)))
+    (provide (box-provide imports))))
+
+(begin-for-syntax
+
+  (define (import->export i)
+    (make-export (import-local-id i)
+      (syntax-e (import-local-id i))
+      (import-mode i)
+      #f
+      (import-orig-stx i)))
+
+  (define (interned-import? i)
+    (symbol-interned? (import-src-sym i))))
+
 (define-syntax box-require
   (make-require-transformer
    (lambda (stx)
@@ -39,15 +51,9 @@
        [(_ ibox spec:expr)
         #:declare ibox (static box? "mutable box for expanded import specs")
         (let-values ([(imports sources) (expand-import #'spec)])
-          (set-box! (syntax-local-value #'ibox) imports)
-          (values imports sources))]))))
-
-(define-for-syntax (import->export i)
-  (make-export (import-local-id i)
-               (syntax-e (import-local-id i))
-               (import-mode i)
-               #f
-               (import-orig-stx i)))
+          (define filtered-imports (filter interned-import? imports))
+          (set-box! (syntax-local-value #'ibox) filtered-imports)
+          (values filtered-imports sources))]))))
 
 (define-syntax box-provide
   (make-provide-transformer
