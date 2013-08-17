516fbea0e09873cc8417eac3a59a15f90eada5a9
Carl Eastlund <cce@racket-lang.org>
2013-08-17 15:50:51 -0400

    Adapted to work both before and after introduction of symbol<?.

--- mischief/symbol.rkt
+++ mischief/symbol.rkt
@@ -13,48 +13,49 @@
   format-symbol)
 
 (require
+  mischief/define
   (only-in racket/bool symbol=?)
   (only-in racket/syntax format-symbol))
 
-(define (symbol-upcase sym)
+(define-if-unbound (symbol-upcase sym)
   (string->symbol
     (string-upcase
       (symbol->string sym))))
 
-(define (symbol-downcase sym)
+(define-if-unbound (symbol-downcase sym)
   (string->symbol
     (string-downcase
       (symbol->string sym))))
 
-(define (symbol-titlecase sym)
+(define-if-unbound (symbol-titlecase sym)
   (string->symbol
     (string-titlecase
       (symbol->string sym))))
 
-(define (symbol-foldcase sym)
+(define-if-unbound (symbol-foldcase sym)
   (string->symbol
     (string-foldcase
       (symbol->string sym))))
 
-(define (symbol<? one two)
+(define-if-unbound (symbol<? one two)
   (and (not (eq? one two))
     (string<?
       (symbol->string one)
       (symbol->string two))))
 
-(define (symbol>? one two)
+(define-if-unbound (symbol>? one two)
   (and (not (eq? one two))
     (string>?
       (symbol->string one)
       (symbol->string two))))
 
-(define (symbol<=? one two)
+(define-if-unbound (symbol<=? one two)
   (or (eq? one two)
     (string<=?
       (symbol->string one)
       (symbol->string two))))
 
-(define (symbol>=? one two)
+(define-if-unbound (symbol>=? one two)
   (or (eq? one two)
     (string>=?
       (symbol->string one)
